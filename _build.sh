1570bc7b8ef84adab2c604103687b1493ac9c380
Joseph Bayly <jtbayly@gmail.com>
2020-09-02 12:32:54 -0400

    generalize mobi creation (fix?)

--- _build.sh
+++ _build.sh
@@ -4,6 +4,6 @@ set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "Sys.setenv(epubFile = bookdown::render_book('index.Rmd', 'bookdown::epub_book'))"
-Rscript -e "bookdown::calibre(Sys.getenv("epubFile"), 'mobi')"
-Rscript -e "Sys.unsetenv("epubFile")"
+Rscript -e "Sys.setenv('epubFile' = bookdown::render_book('index.Rmd', 'bookdown::epub_book'))"
+Rscript -e "bookdown::calibre(Sys.getenv('epubFile'), 'mobi')"
+Rscript -e "Sys.unsetenv('epubFile')"
